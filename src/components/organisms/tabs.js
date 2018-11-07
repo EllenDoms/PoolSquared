8214178a1e6aa00a24ed5bd55506fc5cd158b776
domsellen <domsellen@gmail.com>
2018-11-07 11:20:57 +0100

    tabs css

--- src/components/organisms/tabs.js
+++ src/components/organisms/tabs.js
@@ -6,10 +6,7 @@ export default class Tabs extends Component {
     return (
       <div className="tabs">
       {items.map(item => {
-        console.log(item)
-        return(
-          <div key={item} className={active === item ? 'tab-item active' : 'tab-item'} onClick={() => this.props.action(item)}>{item}</div>
-        )
+        return <div key={item} className={active === item ? 'tab-item active' : 'tab-item'} onClick={() => this.props.action(item)}>{item}</div>
       })}
       </div>
     )
