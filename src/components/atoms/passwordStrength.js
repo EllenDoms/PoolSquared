9b78726cc4190123ba963172533a20cfa2dc03d1
domsellen <domsellen@gmail.com>
2018-11-08 17:11:11 +0100

    no zoom to inputfield

--- src/components/atoms/passwordStrength.js
+++ src/components/atoms/passwordStrength.js
@@ -5,6 +5,7 @@ const hasMixed = value => { return new RegExp(/[a-z]/).test(value) && new RegExp
 const hasSpecial = value => { return new RegExp(/[!#@$%^&*)(+=._-]/).test(value); }
 const strengthIndicator = value => {
    let strengths = 0;
+   if (value.length > 1) { strengths++; }
    if (value.length > 7) { strengths++; }
    if (hasNumber(value)) { strengths++; }
    if (hasSpecial(value)) { strengths++; }
@@ -19,9 +20,9 @@ export default class PasswordStrength extends Component {
     console.log(strength)
     return (
       <div id="passwordStrength">
-        <div className="active strength strength1" />
-        <div className={strength > 0 ? 'active strength strength2' : 'strength strength2' } />
-        <div className={strength > 1 ? 'active strength strength3' : 'strength strength3' } />
+        <div className={strength > 0 ? 'active strength strength1' : 'strength strength1' } />
+        <div className={strength > 1 ? 'active strength strength2' : 'strength strength2' } />
+        <div className={strength > 2 ? 'active strength strength3' : 'strength strength3' } />
       </div>
     )
   }
